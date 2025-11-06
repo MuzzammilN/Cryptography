a106eeb5a2af72c96bc501b11b897b52246bc369
MuzzammilN <naqvimuzzammil@gmail.com>
2025-11-06 01:13:04 -0500

    Working on some stuff -> incomplete

---
+++ StringEncoding.py
@@ -0,0 +1,11 @@
+class String:
+    
+    def __init__(self, ):
+        self.__numberArray = []
+        self.__binaryArray = []
+
+    def __NumberArray(String: str) -> list:
+        for char 
+
+
+    def binary(string: str) -> str:
